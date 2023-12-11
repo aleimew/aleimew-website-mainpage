b37c4b1905b100439b4b5808c7b84faf5e39c425
aleimew <alex@amoore.com>
2023-12-11 13:35:54 -0500

    created tempcode to get basic website deployed as soon as possible.

---
+++ src/Components/Layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import helphim from '../../Assets/helphim.png';
+
+const Layout = () => {
+    return (
+        <div className="App-layout">
+            <h1>Webpage under construction</h1>
+            <img src={helphim} alt="helphim" />
+        </div>
+    );
+};
+
+export default Layout;
