357c1949df8df2df52fbc94947e13e2cd1dc05bc
aleimew <alex@amoore.com>
2024-01-10 15:18:28 -0500

    implimented stylized components for pages

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import helphim from '../Assets/helphim.png';
+import {
+    PageContainer,
+    LeftSide,
+    RightSide,
+    Titles,
+    MyName,
+    Headliners,
+    Quotes,
+} from './pageElements';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Website under construction</h1>
-            <img src={helphim} alt="helphim" />
-        </div>
+        <PageContainer>
+            <LeftSide>
+                <Titles>
+                    <MyName>Alex Moore</MyName>
+                    <Headliners>Pursuing programming, video editing/film making, <br />
+                        game development, help.</Headliners>
+                    <Quotes>"I put my hopes and dreams into this site, You <br />
+                        can tell because I made it myself" - Me
+                    </Quotes>
+                </Titles>
+            </LeftSide>
+
+            <RightSide>
+                <h1>Website under construction</h1>
+                <img src={helphim} alt="helphim" />
+            </RightSide>
+        </PageContainer>
     );
 }
 
---
+++ src/Pages/pageElements.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const LeftSide = styled.div`
+    top: -6rem;
+    right: 13rem;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    color: #000;
+`;
+
+export const RightSide = styled.div`
+    position: relative;
+`;
+
+export const Titles = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+`;
+
+export const MyName = styled.h1`
+    font-size: 5rem;
+    font-weight: 700;
+    margin-bottom: -2rem;
+`;
+
+export const Headliners = styled.h2`
+    font-size: 2rem;
+    font-weight: 350;
+`;
+
+export const Quotes = styled.h3`
+    font-size: 1.5rem;
+    font-style: italic;
+    font-weight: 100;
+    margin-top: -1.5rem;
+    `;
