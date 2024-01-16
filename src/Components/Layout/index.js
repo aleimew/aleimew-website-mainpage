ff2171bdc1bf22487e1ce627ef063a288ac6a165
aleimew <alex@amoore.com>
2024-01-16 17:06:50 -0500

    begun implimentation of mobile version

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import {
     BrowserRouter as Router,
     Routes,
@@ -10,13 +11,30 @@ import Who from '../../Pages/who';
 import How from '../../Pages/how';
 
 const Layout = () => {
+    const [windowDimension, setWindowDimension] = useState(null);
+
+    useEffect(() => {
+        setWindowDimension(window.innerWidth);
+    }, [])
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimension(window.innerWidth);
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
+
+    const mobile = windowDimension <= 912;
+
     return (
         <Router>
             <Routes>
-                <Route exact path="/aleimew-website-mainpage" element={<Home />} />
-                <Route path="/What" element={<What />} />
-                <Route path="/Who" element={<Who />} />
-                <Route path="/How" element={<How />} />
+                <Route exact path="/aleimew-website-mainpage" element={<Home mobile={mobile} />} />
+                <Route path="/What" element={<What mobile={mobile} />} />
+                <Route path="/Who" element={<Who mobile={mobile} />} />
+                <Route path="/How" element={<How mobile={mobile} />} />
             </Routes>
         </Router>
     );
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -11,28 +11,39 @@ import {
     Quotes,
 } from '../Components/PageElements';
 
-
-const Home = () => {
+const PrintNameAndQuotes = () => {
     return (
-        <PageContainer>
-            <LeftSide>
-                <Titles>
-                    <MainTitle>Alex Moore</MainTitle>
-                    <Headliners>Pursuing eveything at once. </Headliners>
-                    <Headliners>Help.</Headliners>
-                    <Quotes>"I put my hopes and dreams into this site. You <br />
-                        can tell because I made it myself" - Me
-                    </Quotes>
-                </Titles>
-            </LeftSide>
+        <Titles>
+            <MainTitle>Alex Moore</MainTitle>
+            <Headliners>Pursuing eveything at once. </Headliners>
+            <Headliners>Help.</Headliners>
+            <Quotes>"I put my hopes and dreams into this site. You <br />
+                can tell because I made it myself" - Me
+            </Quotes>
+        </Titles>
+    );
+}
 
-            <RightSide>
-                <Navbar />
-                <h1>This section is where all my latestest entries <br />
-                    will be added. To be implimented at a later date.</h1>
-                <img src={helphim} alt="helphim" />
-            </RightSide>
-        </PageContainer>
+const Home = (props) => {
+    const mobile = props.mobile;
+    return (
+        <>
+            {!props.mobile ? (
+                <PageContainer>
+                    <LeftSide>
+                        {PrintNameAndQuotes()};
+                    </LeftSide>
+
+                    <RightSide>
+                        <Navbar />
+                        <h1>This section is where all my latestest entries <br />
+                            will be added. To be implimented at a later date.</h1>
+                        <img src={helphim} alt="helphim" />
+                    </RightSide>
+                </PageContainer>
+            ) :
+                (<h1>Mobile Version</h1>)}
+        </>
     );
 }
 
